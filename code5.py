e0301c16d1e46442f8afcd2b6d91d17a34fc92b8
pratibhaaa21 <pratibhajangir1624@gmail.com>
2026-04-10 19:09:52 +0530

    Task 2

--- code5.py
+++
@@ -1,5 +0,0 @@
-def lsb(x):
-    lsb = x & 1
-    print(lsb)
-
-
--- code9.py
+++
@@ -1,3 +0,0 @@
-def extract_bit(pixel_value):
-    return pixel_value & 1  # &1 is the std operator to express the lsb in 0 or 1 .
-
