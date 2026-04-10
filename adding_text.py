871690bf02c918580330ae56da296a4abf8c3053
pratibhaaa21 <pratibhajangir1624@gmail.com>
2026-04-10 15:27:08 +0530

    Code for Task 1

--- adding_text.py
+++
@@ -1,8 +0,0 @@
-# adding a text
-with open("Task 2/2.1/text_file1.txt" ,"w") as file :
-    file.write("Adding text to this txt file using with statement. ")
-
-with open("Task 2/2.1/text_file1.txt" ,"r") as file :
-    content = file.read()
-
-print(content)        
--- finidng_info.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-img = Image.open("Task 2/2.1/image1.jpg")
-print(img.format)
-print(img.size)
-print(img.mode)
