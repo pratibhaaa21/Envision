bb9d9b3d61d0fd3818ba615232aee9b8b4ce2dc2
pratibhaaa21 <pratibhajangir1624@gmail.com>
2026-04-11 11:37:39 +0530

    updated Task 2.1

--- Task 2/2.2/file_sizes.py
+++ Task 2/2.2/file_sizes.py
@@ -6,3 +6,7 @@ size1 = img1.size
 size2 = img2.size
 print(size1 , size2)
 
+'''png reduces size by compressing the data. So if the data is simple colours , shapes etc , png
+compresses it  efficiently . so the size becomes smaller. BUT if the data is a large number of colours
+and very compplex designs etc , png keeps everything unchanges . hence the size becomes larger. '''
+ 
