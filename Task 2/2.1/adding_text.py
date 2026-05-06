c632986c5af136d30de6cc73a0eff77c00a34916
pratibhaaa21 <pratibhajangir1624@gmail.com>
2026-05-06 18:07:16 +0530

    Updated 2.1 files

--- Task 2/2.1/adding_text.py
+++ Task 2/2.1/adding_text.py
@@ -1,8 +1,17 @@
 # adding a text
-with open("Task 2/2.1/text_file1.txt" ,"w") as file :
+with open("Task 2/2.1/text_file1.txt" ,"w") as file :       # w overwrites.
     file.write("Adding text to this txt file using with statement. ")
 
 with open("Task 2/2.1/text_file1.txt" ,"r") as file :
     content = file.read()
 
 print(content)        
+
+
+with open("Task 2/2.1/text_file1.txt" , "a") as file :      # a appends.
+    file.write("\nAdiing another line of text to this file .")    # \n adds this text to the next(new) line.
+
+with open("Task 2/2.1/text_file1.txt" , "r") as file :
+        content1 = file.read()
+
+print (content1)        
